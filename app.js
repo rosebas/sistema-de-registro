5b28dca8be332a5304892b297f9a0884e74312c0
Sebastian <sebasrosales763@gmail.com>
2024-03-19 18:34:36 -0600

    corrije errores de app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const factorial = (n) => {
     if (n === 0 || n === 1){
         return 1;
-    }  else {
+    }  else if (n < 0){
+        return 0;
+    } else {
         return n * factorial(n-1);
     }
 } 
-
 
 numero = prompt("Ingrese numero para calcularle factorial");
 console.log(factorial(numero));
